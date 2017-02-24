fd4d3d6401654651dd91747c3defe2b390dd3af9
Hugo Shaka <hugo.hervieux@rez-gif.supelec.fr>
2017-02-24 21:43:40 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+// Copyright PinaPL
+//
+// main.cpp
+// PinaPL
+//
+
+#include <string.h>
+#include <Eigen/Dense>
+#include <random>
+#include <vector>
+#include <map>
+#include <iostream>
+
+#include "weights.hpp"
+#include "cell.hpp"
+#include "network.hpp"
+
+int main(int argc, char **argv) {
+}
