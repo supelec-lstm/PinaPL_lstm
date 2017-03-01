1fc7a7a0050821a482f55c0faa11496abe5f3068
Sylvain Pascou <spascou@gmail.com>
2017-03-01 20:50:17 +0100

    Test: learn a word n times, then test after the first letter; biases not implemented yet

--- test.cpp
+++ test.cpp
@@ -5,124 +5,80 @@
 //
 
 #include <Eigen/Dense>
+#include <vector>
 #include "weights.hpp"
 #include "cell.hpp"
 #include "test.hpp"
 #include "iostream"
 
 void single_cell_test() {
-    int input_size = 3;
-    int output_size = 5;
+    int input_size = 26;
+    int output_size = 26;
 
-    std::cout << "sequence_size is 2" << std::endl;
-
-    std::cout << "def: input_size = " << std::endl
-    << input_size << std::endl;
-    std::cout << "def: output_size = " << std::endl
-    << output_size << std::endl;
+    std::cout << "sequence_size is 4, word is warp" << std::endl;
+    std::cout
+    << "a b c d e f g h i j k l m n o p q r s t u v w x y z"
+    << std::endl
+    << "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0"
+    << std::endl
+    << "1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0"
+    << std::endl
+    << "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0"
+    << std::endl
+    << "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0"
+    << std::endl;
 
     Weights* cell_weight = new Weights(input_size, output_size);
-    std::cout << "created: cell_weight" << std::endl;
 
     Cell cell = Cell(cell_weight);
-    std::cout << "created: cell; arg: cell_weight" << std::endl;
 
-    std::cout << "value: weight_in_input_gate = " << std::endl
-    << cell.weights->weight_in_input_gate << std::endl;
+    std::vector<Eigen::MatrixXd> inputs;
 
-// FIRST INPUT
-    std::cout << "First caracter in sequence:" << std::endl
-    << "~~~~~" << std::endl;
+    Eigen::MatrixXd inputW(input_size, 1);
+    inputW << 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0;
 
-    Eigen::MatrixXd input1(input_size, 1);
-    input1 << 1, 4, 1;
-    std::cout << "def: input1 = " << std::endl
-    << input1.transpose() << std::endl;
+    Eigen::MatrixXd inputA(input_size, 1);
+    inputA << 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0;
 
-    cell.compute(&input1);
-    std::cout << "computed: cell.compute(&input1)" << std::endl;
+    Eigen::MatrixXd inputR(input_size, 1);
+    inputR << 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0;
 
-    std::cout << "value: input_gate_out = " << std::endl
-    << cell.input_gate_out.back() << std::endl;
+    Eigen::MatrixXd inputP(input_size, 1);
+    inputP << 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0;
 
-    Eigen::MatrixXd output1 = cell.cell_out.back();
-    std::cout << "value: output = " << std::endl
-    << output1.transpose() << std::endl;
+    inputs.push_back(inputW);
+    inputs.push_back(inputA);
+    inputs.push_back(inputR);
+    inputs.push_back(inputP);
 
-    Eigen::MatrixXd expected_output1(output_size, 1);
-    expected_output1 << 5, 1, 1, 8, 5;
-    std::cout << "def: expected_output = " << std::endl
-    << expected_output1.transpose() << std::endl;
+    for (int j=0; j < 10000; j++) {
+      std::vector<Eigen::MatrixXd> deltas;
 
-    Eigen::MatrixXd delta1;
-    delta1 = expected_output1 - output1;
-    std::cout << "value: delta = " << std::endl
-    << delta1.transpose() << std::endl;
+      for (int i=0; i < 4; ++i) {
+        Eigen::MatrixXd input = inputs.at(i);
 
-    std::cout << "End of first caracter in sequence" << std::endl
-    << "~~~~~" << std::endl;
-// END FIRST INPUT
-// SECOND INPUT
-    std::cout << "second caracter in sequence:" << std::endl
-    << "~~~~~" << std::endl;
+        cell.compute(&input);
 
-    Eigen::MatrixXd input2(input_size, 1);
-    input1 << 3, 2, 3;
-    std::cout << "def: input2 = " << std::endl
-    << input2.transpose() << std::endl;
+        Eigen::MatrixXd output = cell.cell_out.back();
 
-    cell.compute(&input2);
-    std::cout << "computed: cell.compute(&input2)" << std::endl;
+        Eigen::MatrixXd delta(output_size, 1);
+        delta = (output-input).cwiseProduct(output-input);
 
-    std::cout << "value: input_gate_out = " << std::endl
-    << cell.input_gate_out.back() << std::endl;
+        deltas.push_back(delta);
+      }
 
-    Eigen::MatrixXd output2 = cell.cell_out.back();
-    std::cout << "value: output = " << std::endl
-    << output2.transpose() << std::endl;
+      for (int i=4-1; i >= 0; --i) {
+        cell.compute_gate_gradient(&deltas.at(i), i);
+      }
 
-    Eigen::MatrixXd expected_output2(output_size, 1);
-    expected_output2 << 4, 5, 3, 1, 1;
-    std::cout << "def: expected_output = " << std::endl
-    << expected_output2.transpose() << std::endl;
+      double lambda = 0.1;
+      cell.update_weights(lambda);
 
-    Eigen::MatrixXd delta2;
-    delta2 = expected_output2 - output2;
-    std::cout << "value: delta2 = " << std::endl
-    << delta2.transpose() << std::endl;
+      cell.reset();
+    }
 
-    std::cout << "End of second caracter in sequence" << std::endl
-    << "~~~~~" << std::endl;
+    cell.compute(&inputW);
 
-
-
-    cell.compute_gate_gradient(&delta2, 1);
-    std::cout << "computed: compute_gate_gradient(&delta, 1)" << std::endl;
-
-    std::cout << "value: delta_input_gate_out = " << std::endl
-    << cell.delta_input_gate_out.back() << std::endl;
-
-    cell.compute_gate_gradient(&delta1, 0);
-    std::cout << "computed: compute_gate_gradient(&delta1, 0)" << std::endl;
-
-    std::cout << "value: delta_input_gate_out = " << std::endl
-    << cell.delta_input_gate_out.back() << std::endl;
-
-    cell.compute_weight_gradient();
-    std::cout << "computed: compute_weight_gradient()" << std::endl;
-
-    std::cout << "value: delta_weight_in_input_gate = " << std::endl
-    << cell.weights->delta_weight_in_input_gate << std::endl;
-
-// APPLY WEIGHTS
-
-    double lambda = 0.5;
-    std::cout << "def: lambda = " << std::endl
-    << lambda << std::endl;
-
-    cell.update_weights(lambda);
-    std::cout << "computed: update_weights(lambda)" << std::endl;
-
-    std::cout << "value: weight_in_input_gate = " << std::endl
-    << cell.weights->weight_in_input_gate << std::endl;
+    std::cout << "Result of learning :" << std::endl;
+    std::cout << cell.cell_out.back() << std::endl;
 }
