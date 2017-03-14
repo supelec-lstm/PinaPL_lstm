3bbb06e3225f94a3741d84a53503598e2c4c1caa
Hugo Shaka <hugo.hervieux@rez-gif.supelec.fr>
2017-03-14 13:52:42 +0100

    Empty test files

---
+++ test.cpp
@@ -0,0 +1,13 @@
+// Copyright PinaPL
+//
+// test.cpp
+// PinaPL
+//
+
+#include <stdlib.h>
+#include <Eigen/Dense>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <map>
+#include "test.hpp"
---
+++ test.hpp
@@ -0,0 +1,21 @@
+// Copyright PinaPL
+//
+// test.hpp
+// PinaPL
+//
+#ifndef TEST_HPP
+#define TEST_HPP
+
+#include <Eigen/Dense>
+#include <vector>
+#include <fstream>
+#include <string>
+#include "weights.hpp"
+#include "cell.hpp"
+#include "test.hpp"
+#include "iostream"
+
+void single_cell_test();
+void single_cell_grammar_test();
+Eigen::MatrixXd get_input(char letter);
+#endif
